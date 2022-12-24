b6862ef5a160638a1c186b6db5482c8504a63a18
prllrp <patricklane09@gmail.com>
2022-12-23 21:00:48 -0600

    Fixed  head

--- pages/index.tsx
+++ pages/index.tsx
@@ -48,7 +48,7 @@ export default function Home() {
   return (
     <Box w='100%'>
       <Head>
-        <title>Create Next App</title>
+        <title>PolyShort</title>
         <meta name="description" content="PolyShort" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
